2463d8e906192c4c109e59cb269f911b3e31ef27
Alex Metcalfe <metcalfe.akj@gmail.com>
2024-11-09 05:13:12 +1100

    refactoring

--- src/app.ts
+++ src/app.ts
@@ -3,18 +3,25 @@ import mongoose from 'mongoose';
 import path from 'path';
 import dotenv from 'dotenv';
 import routes from './routes';
+import rateLimit from 'express-rate-limit';
 
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 3000;
 const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/king_james_bible';
 
 mongoose.connect(MONGODB_URI).then(() => {
-    console.log('Connected to MongoDB');
+  console.log('Connected to MongoDB');
 }).catch(err => {
-    console.error('Failed to connect to MongoDB', err);
+  console.error('Failed to connect to MongoDB', err);
 });
+
+const limiter = rateLimit({
+  windowMs: 6000,
+  max: 100,
+});
+
+app.use(limiter);
 
 app.use(express.static(path.join(__dirname, 'public')));
 
